6ee6e844d99f7b3c78669cf6d77c5be261f4accc
Mengdi Gao <gaomdev@gmail.com>
2015-08-30 10:29:30 +0800

    Move render content closure to function for theme developers

--- press.md.php
+++ press.md.php
@@ -14,12 +14,15 @@ function press_md_inject_assets()
     wp_enqueue_script('press-md-script', plugins_url('assets/js/main.min.js', PRESS_MD_MAIN_FILE), [], false, true);
 }
 
+function press_md_render_the_content($content)
+{
+    $converter = new \League\CommonMark\CommonMarkConverter();
+
+    return $converter->convertToHtml($content);
+}
+
 add_action('admin_print_scripts-post.php', 'press_md_inject_assets');
 add_action('admin_print_scripts-post-new.php', 'press_md_inject_assets');
 add_filter('user_can_richedit', '__return_false');
 
-add_filter('the_content', function ($content) {
-    $converter = new \League\CommonMark\CommonMarkConverter();
-
-    return $converter->convertToHtml($content);
-}, 1);
+add_filter('the_content', 'press_md_render_the_content', 1);
